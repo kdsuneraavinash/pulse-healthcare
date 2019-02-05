30fe423a662a2223f3f69250458f091e2753c72e
K.D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2019-02-05 19:29:41 +0530

    Whoops error handler integrated

--- web/src/Bootstrap.php
+++ web/src/Bootstrap.php
@@ -1,3 +1,29 @@
-<?php declare(strict_types = 1);
+<?php declare(strict_types=1);
 
-echo 'Hello World!';
+namespace Pulse;
+
+/// Autoloader PSR-4
+require __DIR__ . '/../vendor/autoload.php';
+
+/// ========================================================
+/// = Whoops Initialization
+/// ========================================================
+
+error_reporting(E_ALL);
+
+$environment = 'development';
+
+/// Register the error handler
+$whoops = new \Whoops\Run;
+if ($environment !== 'production') {
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function (\Exception $e) {
+        // TODO: Friendly error page and send an email to the developer
+        echo 'Error Occurred in Production Mode: ' . $e->getCode();
+    });
+}
+$whoops->register();
+
+// Test whoops
+// throw new \Exception;
