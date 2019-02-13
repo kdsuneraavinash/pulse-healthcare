2dbd7ec39155619550c5d71d2bd10ca215fb31a3
kdsuneraavinash <kdsuneraavinash@gmail.com>
2019-02-13 23:01:26 +0530

    Fixed Sticky footer bug

--- web/frontend/dist/js/main.js
+++
@@ -1,1 +0,0 @@
-function includeFooterAtBottom(){var main=$("main");var footerH=$("footer").height();var navbarH=$("nav").height()+63;var windowH=$(window).height();var mainH=main.height();if(mainH<windowH-(footerH+navbarH)){main.height(windowH-(footerH+navbarH));}else{main.height("auto");}};$(window).resize(includeFooterAtBottom);$(document).ready(includeFooterAtBottom);
--- web/frontend/src/js/login.js
+++ web/frontend/src/js/login.js
@@ -5,7 +5,7 @@
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
         var forms = $('.needs-validation');
         // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function (form) {
+        Array.prototype.filter.call(forms, function (form) {
             form.addEventListener('submit', function (event) {
                 if (form.checkValidity() === false) {
                     event.preventDefault();
