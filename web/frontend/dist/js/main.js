a42ebcd82e997a729ff9a87a1e0cba2e8bcc9ef1
kdsuneraavinash <kdsuneraavinash@gmail.com>
2019-02-14 07:10:00 +0530

    Added medical center form validation

--- web/frontend/dist/js/main.js
+++
@@ -1,1 +0,0 @@
-'use strict';(function(){window.addEventListener('load',function(){var forms=$('.needs-validation');Array.prototype.filter.call(forms,function(form){form.addEventListener('submit',function(event){if(form.checkValidity()===false){event.preventDefault();event.stopPropagation();}else{form.submit();};form.classList.add('was-validated');},false);});},false);})();
