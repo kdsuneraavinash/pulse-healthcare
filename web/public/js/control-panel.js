0c486384d2fd53615ad9b2088b4c1e1a2fe1dfb1
kdsuneraavinash <kdsuneraavinash@gmail.com>
2019-02-22 06:54:55 +0530

    Styled admin control panel

--- web/public/js/control-panel.js
+++ web/public/js/control-panel.js
@@ -12,18 +12,22 @@ $("#drawer-close").click(function (e) {
 $(".sidebar-button").click(
     function () {
         let buttons = $(".sidebar-button");
-        buttons.removeClass("active");
-        buttons.removeClass("rounded");
-        buttons.removeClass("mb-0");
-        buttons.removeClass("z-depth-2");
+        let iframe = $("#content-iframe");
 
-        $(this).toggleClass("active");
-        $(this).toggleClass("rounded");
-        $(this).toggleClass("mb-0");
-        $(this).toggleClass("z-depth-2");
+        let newLoc = window.location.origin + window.location.pathname + '/' + $(this).attr('id');
 
-        $("#content-iframe").attr('src', window.location.origin + window.location.pathname + '/' + $(this).attr('id'));
+        if (iframe.attr("src") !== newLoc){
+            buttons.removeClass("active");
+            buttons.removeClass("rounded");
+            buttons.removeClass("mb-0");
+            buttons.removeClass("z-depth-2");
 
-        console.log();
+            $(this).toggleClass("active");
+            $(this).toggleClass("rounded");
+            $(this).toggleClass("mb-0");
+            $(this).toggleClass("z-depth-2");
+
+            iframe.attr('src', newLoc);
+        }
     }
 );
