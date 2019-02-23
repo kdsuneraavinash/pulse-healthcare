881341de7164e4373e5a5b22650ea7dd721315dc
kdsuneraavinash <kdsuneraavinash@gmail.com>
2019-02-23 21:48:15 +0530

    Added real time validation to login and doctor registration

--- web/public/js/register-doctor.js
+++ web/public/js/register-doctor.js
@@ -1,52 +1,79 @@
 'use strict';
 
+let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 /// Validate forms in the page
 $(document).ready(function () {
     let forms = $('.needs-validation');
 
+    let fullName = $("#full_name");
+    let displayName = $("#display_name");
+    let category = $("#category");
+    let slmcId = $("#slmc_id");
+    let email = $("#email");
+    let phoneNumber = $("#phone_number");
+    let nic = $("#nic");
+
+    fullName.on("input", function () {
+        validateNotEmpty(fullName);
+    });
+    displayName.on("input", function () {
+        validateNotEmpty(displayName);
+    });
+    category.on("input", function () {
+        validateNotEmpty(category);
+    });
+    slmcId.on("input", function () {
+        validateNotEmpty(slmcId);
+    });
+    email.on("input", function () {
+        validateNotEmptyAndRegex(email, emailRegex);
+    });
+    phoneNumber.on("input", function () {
+        validateNotEmpty(phoneNumber);
+    });
+    nic.on("input", function () {
+        validateNotEmpty(nic);
+    });
+
     Array.prototype.filter.call(forms, function (form) {
-        form.addEventListener('submit', function (event) {
-            let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-
-            function checkField(field, conditions) {
-                field.removeClass("is-valid");
-                field.removeClass("is-invalid");
-                if (conditions) {
-                    field.addClass("is-valid");
-                    return true;
-                } else {
-                    field.addClass("is-invalid");
-                    event.preventDefault();
-                    event.stopPropagation();
-                    return false;
-                }
-            }
-
-            let fullName = $("#full_name");
-            let displayName = $("#display_name");
-            let category = $("#category");
-            let slmcId = $("#slmc_id");
-            let email = $("#email");
-            let phoneNumber = $("#phone_number");
-            let nic = $("#nic");
-
-            let fullNameValid = checkField(fullName, fullName.val() !== "");
-            let displayNameValid = checkField(displayName, displayName.val() !== "");
-            let categoryValid = checkField(category, category.val() !== "");
-            let slmcIdValid = checkField(slmcId, slmcId.val() !== "");
-            let emailValid = checkField(email, email.val() !== "" &&
-                emailRegex.test(email.val()));
-            let phoneNumberValid = checkField(phoneNumber, phoneNumber.val() !== "");
-            let nicValid = checkField(nic, nic.val() !== "");
-
-            if (fullNameValid && displayNameValid && categoryValid && slmcIdValid && emailValid && phoneNumberValid &&
-                nicValid) {
+        form.addEventListener('submit', function () {
+            if (validateNotEmpty(fullName) &&
+                validateNotEmpty(displayName) &&
+                validateNotEmpty(category) &&
+                validateNotEmpty(slmcId) &&
+                validateNotEmpty(email, emailRegex) &&
+                validateNotEmpty(phoneNumber) &&
+                validateNotEmpty(nic)) {
                 form.submit();
             }
         }, false);
     });
 });
+
+
+function checkField(field, conditions) {
+    field.removeClass("is-valid");
+    field.removeClass("is-invalid");
+    if (conditions) {
+        field.addClass("is-valid");
+        return true;
+    } else {
+        field.addClass("is-invalid");
+        event.preventDefault();
+        event.stopPropagation();
+        return false;
+    }
+}
+
+function validateNotEmpty(field) {
+    return checkField(field, field.val() !== "");
+}
+
+function validateNotEmptyAndRegex(field, regex) {
+    return checkField(field, field.val() !== "" &&
+        regex.test(field.val()));
+}
 
 $(function () {
     $('[data-toggle="popover"]').popover()
