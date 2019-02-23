8a1ecc7ad84530b4222ca038e91d4c12518324af
Lahiru <lahirudesilva.17@cse.mrt.ac.lk>
2019-02-23 13:25:55 +0530

    Added PatientDetails.php file

---
+++ web/src/Models/Patient/PatientDetails.php
@@ -0,0 +1,183 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lahiru
+ * Date: 2/23/19
+ * Time: 11:33 AM
+ */
+
+namespace Pulse\Models\Patient;
+use DB;
+use Pulse\Exceptions\AccountNotExistException;
+
+class PatientDetails{
+    private $name;
+    private $nic;
+    private $email;
+    private $phoneNumber;
+    private $address;
+    private $postalId;
+
+    /**
+     * PatientDetails constructor.
+     * @param $name
+     * @param $nic
+     * @param $email
+     * @param $phoneNumber
+     * @param $address
+     * @param $postalCode
+     */
+    public function __construct($name, $nic, $email, $phoneNumber, $address, $postalCode)
+    {
+        $this->name = $name;
+        $this->nic = $nic;
+        $this->email = $email;
+        $this->phoneNumber = $phoneNumber;
+        $this->address = $address;
+        $this->postalCode = $postalCode;
+    }
+
+    public function validate(){
+        $nameValid = $this->name != '' && (preg_match("/^([a-zA-Z' ]+)$/",$this->name));
+        $nicValid = $this->nic != '' && ((preg_match('~^[A-Z]\d{9}$~', $this->nic)) || strlen((string)$this->nic)==12);
+        $emailValid = $this->email != "" && preg_match(' /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*' .
+                ')|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|' .
+                '(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/', $this->email);
+        $phoneNumberValid = $this->phoneNumber != "" && (strlen((string)$this->phoneNumber)==10);
+        $addressValid = $this->address != "";
+        $postalValid = $this->postalCode != "";
+        return $nameValid && $nicValid && $emailValid && $phoneNumberValid && $addressValid && $postalValid;
+
+    }
+
+    /**
+     * @param string $accountId
+     * @return PatientDetails
+     * @throws AccountNotExistException
+     */
+    public static function readFromDatabse(string $accountId):PatientDetails
+    {
+        $query = DB::queryFirstRow("SELECT * FROM patient_details WHERE account_id=%s", $accountId);
+        if ($query == null) {
+            throw new AccountNotExistException($accountId);
+        }
+        return new PatientDetails($query['name'], $query['nic'], $query['email'], $query['phone_number'],
+            $query['address'], $query['postal_code']);
+
+    }
+
+
+    public function saveInDatabase(string $accountId)
+    {
+        DB::insert('patient_details', array(
+            'account_id' => $accountId,
+            'name' => $this->getName(),
+            'nic' => $this->getNic(),
+            'email' => $this->getEmail(),
+
+            'phone_number' => $this->getPhoneNumber(),
+            'address' => $this->getAddress(),
+            'postal_code' => $this->getPostalCode()
+        ));
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNic()
+    {
+        return $this->nic;
+    }
+
+    /**
+     * @param mixed $nic
+     */
+    public function setNic($nic): void
+    {
+        $this->nic = $nic;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhoneNumber()
+    {
+        return $this->phoneNumber;
+    }
+
+    /**
+     * @param mixed $phoneNumber
+     */
+    public function setPhoneNumber($phoneNumber): void
+    {
+        $this->phoneNumber = $phoneNumber;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param mixed $address
+     */
+    public function setAddress($address): void
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPostalCode()
+    {
+        return $this->postalCode;
+    }
+
+    /**
+     * @param mixed $postalId
+     */
+    public function setPostalCode($postalCode): void
+    {
+        $this->postalId = $postalCode;
+    }
+
+
+}
+
+
