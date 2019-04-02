3a947c35b78e1446cbdce24679650b76e9a22ebc
K.D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2019-04-02 05:31:41 +0530

    Improved profile page

--- web/src/Controllers/ProfilePageController.php
+++ web/src/Controllers/ProfilePageController.php
@@ -3,6 +3,7 @@
 namespace Pulse\Controllers;
 
 use Pulse\Models\AccountSession\Account;
+use Pulse\Models\Admin\Admin;
 use Pulse\Models\Doctor\Doctor;
 use Pulse\Models\Exceptions\AccountNotExistException;
 use Pulse\Models\Exceptions\AccountRejectedException;
@@ -21,34 +22,32 @@ class ProfilePageController extends BaseController
     {
         $current_account = $this->getCurrentAccount();
         $context = array();
-        $context['account_id'] = $this->getCurrentAccount()->getAccountId();
-        $context['account_type'] = $this->getCurrentAccount()->getAccountType();
-        if($current_account instanceof Patient){
-            $context['name']=$current_account->getPatientDetails()->getName();
-            $context['nic']=$current_account->getPatientDetails()->getNic();
-            $context['email']=$current_account->getPatientDetails()->getEmail();
-            $context['phone_number']=$current_account->getPatientDetails()->getPhoneNumber();
-            $context['address']=$current_account->getPatientDetails()->getAddress();
-            $context['postal_code']=$current_account->getPatientDetails()->getPostalCode();
-        }else if($current_account instanceof MedicalCenter){
-            $context['name']=$current_account->getMedicalCenterDetails()->getName();
-            $context['phsrc']=$current_account->getMedicalCenterDetails()->getPhsrc();
-            $context['email']=$current_account->getMedicalCenterDetails()->getEmail();
-            $context['fax']=$current_account->getMedicalCenterDetails()->getFax();
-            $context['phone_number']=$current_account->getMedicalCenterDetails()->getPhoneNumber();
-            $context['address']=$current_account->getMedicalCenterDetails()->getAddress();
-            $context['postal_code']=$current_account->getMedicalCenterDetails()->getPostalCode();
-        }else if($current_account instanceof Doctor){
-            $context['name']=$current_account->getDoctorDetails()->getDisplayName();
-            $context['full_name']=$current_account->getDoctorDetails()->getFullName();
-            $context['slmc_id']=$current_account->getDoctorDetails()->getSlmcId();
-            $context['nic']=$current_account->getDoctorDetails()->getNic();
-            $context['category']=$current_account->getDoctorDetails()->getCategory();
-            $context['email']=$current_account->getDoctorDetails()->getEmail();
-            $context['phone_number']=$current_account->getDoctorDetails()->getPhoneNumber();
-
+        if ($current_account instanceof Patient) {
+            $context['name'] = $current_account->getPatientDetails()->getName();
+            $context['nic'] = $current_account->getPatientDetails()->getNic();
+            $context['email'] = $current_account->getPatientDetails()->getEmail();
+            $context['phone_number'] = $current_account->getPatientDetails()->getPhoneNumber();
+            $context['address'] = $current_account->getPatientDetails()->getAddress();
+            $context['postal_code'] = $current_account->getPatientDetails()->getPostalCode();
+        } else if ($current_account instanceof MedicalCenter) {
+            $context['name'] = $current_account->getMedicalCenterDetails()->getName();
+            $context['phsrc'] = $current_account->getMedicalCenterDetails()->getPhsrc();
+            $context['email'] = $current_account->getMedicalCenterDetails()->getEmail();
+            $context['fax'] = $current_account->getMedicalCenterDetails()->getFax();
+            $context['phone_number'] = $current_account->getMedicalCenterDetails()->getPhoneNumber();
+            $context['address'] = $current_account->getMedicalCenterDetails()->getAddress();
+            $context['postal_code'] = $current_account->getMedicalCenterDetails()->getPostalCode();
+        } else if ($current_account instanceof Doctor) {
+            $context['name'] = $current_account->getDoctorDetails()->getDisplayName();
+            $context['full_name'] = $current_account->getDoctorDetails()->getFullName();
+            $context['slmc_id'] = $current_account->getDoctorDetails()->getSlmcId();
+            $context['nic'] = $current_account->getDoctorDetails()->getNic();
+            $context['category'] = $current_account->getDoctorDetails()->getCategory();
+            $context['email'] = $current_account->getDoctorDetails()->getEmail();
+            $context['phone_number'] = $current_account->getDoctorDetails()->getPhoneNumber();
+        } else if ($current_account instanceof Admin)  {
+            $context['name'] = "Administrator";
         }
-        //parent::loadOnlyIfUserIsOfType(Account::class, 'ProfilePage.html.twig',array("context"=>$context));
-        $this->render("ProfilePage.html.twig",array("context"=>$context),$current_account);
+        $this->render("ProfilePage.html.twig", $context, $current_account);
     }
 }
